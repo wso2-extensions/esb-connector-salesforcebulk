721e692abdbed9793a3b5813209f26ba957bc738
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-08 18:00:02 +0530

    After releasing the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/salesforcebulk/SalesforceBulkConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/salesforcebulk/SalesforceBulkConnectorIntegrationTest.java
@@ -51,7 +51,7 @@ public class SalesforceBulkConnectorIntegrationTest extends ConnectorIntegration
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("salesforcebulk-connector-1.0.1");
+        init("salesforcebulk-connector-1.0.2-SNAPSHOT");
         String apiVersion = connectorProperties.getProperty("apiVersion");
         apiUrl = connectorProperties.getProperty("apiUrl") + "/services/async/" + apiVersion;
 
